3671a1b1bb8c0cf9b62f28da41b86c1342c6362a
won <wjin0352@gmail.com>
2017-02-19 21:23:33 -0500

    readme and store.js

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-npm install
-npm start
+clean template for react redux project complete with basic server code with mongodb backend.
+
+To Start just run: 
+>npm install
+>npm start
---
+++ src/store.js
@@ -0,0 +1,16 @@
+var redux = require('redux');
+import { createStore, compose, applyMiddleware } from 'redux';
+import allReducers from './reducers/index';
+var thunk = require('redux-thunk').default;
+// logging tool for debugging
+import createLogger from 'redux-logger';
+
+const logger = createLogger();
+const store = createStore(allReducers,
+  compose(
+    applyMiddleware(thunk, logger),
+    window.devToolsExtension ? window.devToolsExtension() : f => f
+  )
+);
+
+export default store;
